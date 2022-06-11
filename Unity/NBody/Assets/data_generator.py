c48d53288c3835d8a934bd6a416535dc5ad450d4
nkovacic99 <nace.kovacic007@gmail.com>
2022-06-11 16:13:43 +0200

    project reads from .csv & .csv data generator added

---
+++ Unity/NBody/Assets/data_generator.py
@@ -0,0 +1,40 @@
+import pandas as pd 
+import random
+
+
+def generate(lower_bound:float, upper_bound:float, sample_size:int, precision=5, filename="data.csv"):
+    """
+    Generates a .csv file with following columns:
+        - positionX
+        - positionY
+        - positionZ
+        - velocityX
+        - velocityY
+        - velocityZ
+        - mass
+    All represent float numbers.
+
+    sample_size: number of rows in .csv (excluding header)  
+    """ 
+    data = {
+        "positionX": [],
+        "positionY": [],
+        "positionZ": [],
+        "velocityX": [],
+        "velocityY": [],
+        "velocityZ": [],
+        "mass": [],
+    }
+
+    while (sample_size > 0):
+        for column in data:
+            randfloat = round(random.uniform(lower_bound, upper_bound), precision)
+            if column == "mass":
+                randfloat = abs(randfloat)
+            data[column].append(randfloat)
+        sample_size -= 1
+
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
+generate(-50, 50, 1000)
